5c4a5076fc46d16c013df4a60973db226a00bada
kora1348 <amelianekor@gmail.com>
2025-05-02 15:20:41 +0200

    toto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OnlineModule } from './views/online/online.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    OnlineModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
